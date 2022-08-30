a1b90707ca7fbf45526098c9547b8acbb57690a3
Md Abdullah <spark8ab@gmail.com>
2022-08-30 17:32:14 +0600

    88 ends here. adding action and compiling code.

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,28 +3,10 @@ import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 
-class TransactionList extends StatefulWidget {
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
 
-
-  @override
-  State<TransactionList> createState() => _TransactionListState();
-}
-
-class _TransactionListState extends State<TransactionList> {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Groceries',
-      amount: 16.53,
-      date: DateTime.now(),
-    ),
-  ];
+  TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
